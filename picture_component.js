5af1ab29e1c6c8db22e38b27f517f5d48295bcd0
taishikamiya <t.kamiya@edu.ciid.dk>
2017-11-22 14:04:52 +0100

    update

--- picture_component.js
+++ picture_component.js
@@ -10,22 +10,22 @@ export class PictureComponent {
   }};
   private captureData: string;
 
-  constructor(
+  function constructor(
     private renderer: Renderer2,
     private apiService: ApiService,
   ) { }
 
-  ngAfterViewInit() {
+  function ngAfterViewInit() {
     this.startCamera();
   }
 
-  onClick() {
+  function onClick() {
     this.captureData = this.draw();
     this.stopCamera();
     this.savePicture();
   }
 
-  private draw() {
+  private function draw() {
     // 写真のサイズを決める
     const WIDTH = this.videoElm.nativeElement.clientWidth;
     const HEIGHT = this.videoElm.nativeElement.clientHeight;
@@ -39,7 +39,7 @@ export class PictureComponent {
     return this.canvasElm.nativeElement.toDataURL(ctx.drawImage(this.videoElm.nativeElement, 0, 0, WIDTH, HEIGHT));
   }
 
-  private startCamera() {
+  private function startCamera() {
     window.navigator.mediaDevices.getUserMedia(this.medias)
       .then(stream => this.videoElm.nativeElement.srcObject = stream)
       .catch(error => {
@@ -48,13 +48,13 @@ export class PictureComponent {
       });
   }
 
-  private stopCamera() {
+  private function stopCamera() {
     this.videoElm.nativeElement.pause();
     const track = this.videoElm.nativeElement.srcObject.getTracks()[0] as MediaStreamTrack;
     track.stop();
   }
 
-  private savePicture() {
+  private function savePicture() {
     this.apiService.savePicture(this.captureData);
 
     const a = this.renderer.createElement('a') as HTMLAnchorElement;
